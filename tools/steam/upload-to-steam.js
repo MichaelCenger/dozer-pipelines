6c9bd4aa8cb66072556c2aa295d32684280e6453
Michael Cenger <MichaelCenger@gmail.com>
2022-07-23 00:01:25 +0200

    Remove 2FA code step

--- tools/steam/upload-to-steam.js
+++ tools/steam/upload-to-steam.js
@@ -1,11 +1,9 @@
 import { spawn } from 'child_process'
 
-let code = process.env['CI_STEAM_2FA_CODE']
 let login = process.env['CI_STEAM_LOGIN']
 let password = process.env['CI_STEAM_PASSWORD']
 let vdf = process.env['CI_STEAM_VDF']
 let builderDir = process.env['CI_STEAM_BUILDER_DIR']
-
 let executable = '.\\steamcmd.exe'
 let args = [
   '+login', 
@@ -22,8 +20,3 @@ let cmd = spawn(executable, args, { cwd: builderDir })
 
 cmd.stdout.on('data', (data) => console.log('stdout: ' + data.toString()) )
 cmd.stderr.on('data', (data) => console.log('stderr: ' + data.toString()) )
-
-setTimeout(() => {
-  console.log('Piping the code in...', code)
-  cmd.stdin.write(`${code}\n`)
-}, 5000)
